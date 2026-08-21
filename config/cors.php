5dbd7624452e79dd6f34ed3a21e74a6fa1f94855
m4nt3k1ll4 <juanchin23ad@gmail.com>
2026-08-21 15:48:06 -0500

    feat(cors): add additional allowed origins for CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -18,9 +18,12 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'http://localhost:4200',
+        'http://127.0.0.1:4200',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'http://localhost:3000',
         'https://intranet.insummabg.net',
     ],
 
